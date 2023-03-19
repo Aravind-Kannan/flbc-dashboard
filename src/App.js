af558fd1f75dd3775bd7ec1ec1de874bc64de666
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2023-03-19 10:43:50 +0530

    :sparkles: feat: built basic prototype

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import "./App.css";
+import contractABI from "./ModelMetricStoreContractABI.json";
+
+const DECIMAL_PART = 100_000;
+const URL = "http://localhost:7545";
+const CONTRACT_ADDRESS = "0x7558BE17824B3C943eC338D3138Dc958585783e7";
 
 function App() {
+  const [loss, setLoss] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const provider = new ethers.JsonRpcProvider(URL);
+      const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        contractABI,
+        provider
+      );
+
+      const _loss = await contract.getLoss();
+      const _accuracy = await contract.getAccuracy();
+      console.log(_loss, _accuracy);
+      setLoss((Number(_loss) / DECIMAL_PART) * 100);
+      setAccuracy((Number(_accuracy) / DECIMAL_PART) * 100);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Loss: {loss} %</h1>
+        <h1>Accuracy: {accuracy} %</h1>
       </header>
     </div>
   );
