201e657bfa8cb9a6c5ccd38f7babd65814f6025c
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2023-03-23 10:19:34 +0530

    :sparkles: feat: add logs for Hash Storage Smart contracts

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import "./App.css";
 import modelABI from "./ModelABI.json";
-import hashStorageABI from "./HashStorageABI.json";
+import hashStorageLocalABI from "./HashStorageLocalABI.json";
+import hashStorageGlobalABI from "./HashStorageGlobalABI.json";
 
 const DECIMAL_PART = 100_000;
 const URL = "http://localhost:7545";
 const contracts = {
   model: {
-    contractAddress: "0x1af24D3847D4EF774Ce27B6da860AacF3d043c42",
+    contractAddress: "0x3CFdd17873bD4765f4b6328deE06F692aB32E0e0",
     ABI: modelABI,
   },
-  hashStorage: {
-    contractAddress: "0x8124ad03A38F20abC51279aBCdF9cb4f6589EF97",
-    ABI: hashStorageABI,
+  hashStorageLocal: {
+    contractAddress: "0x499d3cA782E12E530a1cE0A55E14181dBbd45F07",
+    ABI: hashStorageLocalABI,
+  },
+  hashStorageGlobal: {
+    contractAddress: "0xaccFC90d28D3D3c3271b20675561CeDC6040F9D8",
+    ABI: hashStorageGlobalABI,
   },
 };
 
@@ -29,9 +34,14 @@ function App() {
         contracts.model.ABI,
         provider
       );
-      const hashStorageContract = new ethers.Contract(
-        contracts.hashStorage.contractAddress,
-        contracts.hashStorage.ABI,
+      const hashStorageLocalContract = new ethers.Contract(
+        contracts.hashStorageLocal.contractAddress,
+        contracts.hashStorageLocal.ABI,
+        provider
+      );
+      const hashStorageGlobalContract = new ethers.Contract(
+        contracts.hashStorageGlobal.contractAddress,
+        contracts.hashStorageGlobal.ABI,
         provider
       );
 
@@ -42,15 +52,18 @@ function App() {
       setLoss((Number(_loss) / DECIMAL_PART) * 100);
       setAccuracy((Number(_accuracy) / DECIMAL_PART) * 100);
 
-      const _hash = await hashStorageContract.getHash();
-      console.log(_hash);
       const modelLogs = await modelContract.queryFilter(
         "ValuesUpdated",
         0,
         "latest"
       );
 
-      const hashStorageLogs = await hashStorageContract.queryFilter(
+      const hashStorageLocalLogs = await hashStorageLocalContract.queryFilter(
+        "HashUpdated",
+        0,
+        "latest"
+      );
+      const hashStorageGlobalLogs = await hashStorageGlobalContract.queryFilter(
         "HashUpdated",
         0,
         "latest"
@@ -58,7 +71,7 @@ function App() {
 
       modelLogs.forEach((log, i) => {
         const parsedLog = modelContract.interface.parseLog(log);
-        console.log(i);
+        console.log("modelLogs", i);
         console.log(
           "Timestamp:",
           new Date(Number(parsedLog.args[0]) * 1000).toLocaleString()
@@ -68,14 +81,26 @@ function App() {
         console.log("Application:", parsedLog.args[3]);
       });
 
-      hashStorageLogs.forEach((log, i) => {
-        const parsedLog = hashStorageContract.interface.parseLog(log);
-        console.log(i);
+      hashStorageLocalLogs.forEach((log, i) => {
+        const parsedLog = hashStorageLocalContract.interface.parseLog(log);
+        console.log("hashStorageLocalLogs", i);
         console.log(
           "Timestamp:",
           new Date(Number(parsedLog.args[0]) * 1000).toLocaleString()
         );
         console.log("Hash:", parsedLog.args[1]);
+        console.log("Application:", parsedLog.args[2]);
+      });
+
+      hashStorageGlobalLogs.forEach((log, i) => {
+        const parsedLog = hashStorageGlobalContract.interface.parseLog(log);
+        console.log("hashStorageGlobalLogs", i);
+        console.log(
+          "Timestamp:",
+          new Date(Number(parsedLog.args[0]) * 1000).toLocaleString()
+        );
+        console.log("Hash:", parsedLog.args[1]);
+        console.log("Application:", parsedLog.args[2]);
       });
       return () => clearInterval(interval);
     }, 1000);
